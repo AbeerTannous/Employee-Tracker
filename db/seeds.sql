b68ddd37af2869dc4d388b078764539e585b3d74
Abeer Tannous <{abeer.tannous}@yahoo.com>
2021-12-15 11:45:33 -0500

    packages installaton

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES
+('IT'),
+('Finance'),
+('Sales'),
+('Customer Service');
+
+INSERT INTO role(title,salary,department_id)
+VALUES
+('Software Engineer',60000,1),
+('Accountant',50000,2),
+('Sales Manager',60000,3),
+('Operator',40000,4);
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id)
+VALUES
+('Mark','Firbank',1,5),
+('Virginia','Woolf',2,null),
+('Charles','LeRoi',3,6),
+('Dora','Carrington',4,null);
